1be91e5cc860559c7486f94b20eec9e4fe8e5c77
Jake Coppinger <jake@jakecoppinger.com>
2015-11-17 09:42:53 +1100

    Added new default text and style

--- app/js/controllers/setup_markdown.js
+++ app/js/controllers/setup_markdown.js
@@ -5,7 +5,7 @@ Jake Coppinger 2015
 
 function setupMarkdown(scope) {
     // Set up Markdown
-    scope.markdown = "# Designless.io\n*If you can use an emoticon, you can write Markdown.*\n\nDeployed frequently at [Designless.io](http://www.designless.io).\nBuilt by [Jake Coppinger](http://www.jakecoppinger.com).\n\n## Example textbox\n**This is an example textbox**\nTextboxes are created with a heading, denoted with a hash (or number of hashes\n\nSee the Markdown Guide in the menu for more information";
+    scope.markdown = "# Designless.io\n*If you can use an emoticon, you can write Markdown.*\n\nDeployed at [Designless.io](http://www.designless.io).\nBuilt by [Jake Coppinger](http://www.jakecoppinger.com).\n\n## Example textbox\n**This is an example textbox**\nMarkdown is a simple way of writing without distractions. Textboxes are created with a heading, denoted with a hash (or number of hashes).\n\nSee the Markdown Guide in the menu for more information";
 
     // Set up layout
     var layoutJSON;//  = Lockr.get('layoutjson');
@@ -14,7 +14,7 @@ function setupMarkdown(scope) {
     if (layoutJSON) {
         layout = JSON.parse(layoutJSON);
     } else {
-        layoutJSON = '{"document":{"height":210,"width":297,"layout":"absolute"},"boxes":{"Designless.io":{"size":{"width":100,"height":100},"position":{"page":1,"left":0,"top":0},"style":"Default style"},"Example textbox":{"size":{"width":100,"height":100},"position":{"page":2,"left":56,"top":56},"style":"Helvetica"}},"styles":{"Default style":{"font":"Comic Sans S","color":"rgb(137, 48, 57)"},"Papyrus":{"font":"Papyrus","color":"rgb(137, 48, 57)"},"Helvetica":{"font":"Helvetica Neue","color":"#000000"}},"fonts":["Arial","Helvetica","HelveticaNeue","Papyrus","Georgia","Garamond","Gill Sans"]}';
+        layoutJSON = '{"document":{"height":210,"width":297,"layout":"absolute"},"boxes":{"Designless.io":{"size":{"width":136.26591698362824,"height":117.21514800727634},"position":{"page":1,"top":24.07805523400033,"left":36.513973871341165},"style":"Default style"},"Example textbox":{"size":{"width":100.81031916652886,"height":66.41309740367124},"position":{"page":1,"top":141.29320324127667,"left":36.513973871341165},"style":"Example Style"}},"styles":{"Default style":{"font":"Helvetica","color":"rgb(0, 0, 0)","fontsize":"26.112","lineheight":"1.147","headingsize":"56.925","fontweight":"0","textalign":"left"},"Example Style":{"font":"Helvetica","color":"rgb(86, 85, 85)","fontsize":"15.943","lineheight":"1.062","opacity":"1","headingsize":"0","textalign":"left"}},"fonts":["Arial","Helvetica","HelveticaNeue","Papyrus","Georgia","Garamond","Gill Sans"]}';
 
         //'{"document":{"height":210,"width":297,"layout":"absolute"},"boxes":{},"styles":{"Default":{"font":"Arial"}}}';
 
